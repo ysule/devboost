9e094ab9df5fa784f06c6b6a5ced69e93ec72d5d
yayati.sule <yayati@open-insights.com>
2020-07-11 19:28:20 +0530

    added entries for resources folder in archetype definitions

--- spark-scala-simple/src/main/resources/archetype-resources/src/main/scala/App.scala
+++ spark-scala-simple/src/main/resources/archetype-resources/src/main/scala/App.scala
@@ -1,15 +1,18 @@
 package ${package}
 
+import org.apache.spark.sql.SparkSession
 /**
  * @author ${user.name}
  */
 object App {
 
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-
   def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+    val spark: SparkSession = SparkSession.builder.getOrCreate()
+    val data:Array[Int] = Array(1,2,3,4,5)
+    val rdd = spark.sparkContext.parallelize(data)
+    println(rdd.take(2))
+    println(rdd.map(_*2).take(2))
+    spark.stop()          
   }
 
 }
